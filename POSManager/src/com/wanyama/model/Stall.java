5e0ac1cf598f8276495e9f9ed2f8d973fe8a00bd
wyllos <will.i.yeezy@gmail.com>
2014-04-01 16:36:20 +0200

    added intial POS stall class template

--- POSManager/src/com/wanyama/model/Stall.java
+++ POSManager/src/com/wanyama/model/Stall.java
@@ -1,5 +1,34 @@
 package com.wanyama.model;
 
+/*
+ *Class representation of sale booths (points of sale) 
+ * @author FUJITSU
+ *
+ */
 public class Stall {
-
+	
+	int id;
+	int table_number;
+	
+	// constructor
+	public Stall(int id, int number) {
+		this.id = id;
+		table_number = number;
+	}
+	
+	// Setters
+	public void setId(int id){
+		this.id = id;
+	}
+	public void setNumber(int number){
+		table_number = number;
+	}
+	
+	// Getters
+	public int getId(){
+		return id;
+	}
+	public int getNumber(){
+		return table_number;
+	}
 }
