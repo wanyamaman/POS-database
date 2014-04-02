895dcf392f276cfabb9671451a01707810ebf9c2
wyllos <will.i.yeezy@gmail.com>
2014-04-02 14:07:05 +0200

    definied CRUD definition for stalls

--- POSManager/src/com/wanyama/helper/MasterDatabaseHelper.java
+++ POSManager/src/com/wanyama/helper/MasterDatabaseHelper.java
@@ -1,8 +1,17 @@
 package com.wanyama.helper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.wanyama.model.Product;
+import com.wanyama.model.Stall;
 
 public class MasterDatabaseHelper extends SQLiteOpenHelper {
 
@@ -74,7 +83,99 @@ public class MasterDatabaseHelper extends SQLiteOpenHelper {
 		
 		// create new tables
 		onCreate(db);
-
+	}
+	
+	/********************************************************
+	 * CRUD (Create, Read, Update, Delete) Method definitions
+	 *********************************************************/
+	
+	// create a stall
+	public long createStall(Stall stall, long[] product_ids){
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(KEY_STALL_NUMBER, stall.getNumber());	//booth number
+		values.put(KEY_STALL_BALANCE, stall.getBalance());	//stand bill
+		
+		// primary key generated
+		long stall_id = db.insert(TABLE_STALLS, null, values);
+		
+		return stall_id;
+	}
+	
+	// fetch a stall
+	public Stall getStall(long stall_id){
+		SQLiteDatabase db = this.getReadableDatabase();
+		
+		String selectQuery = "SELECT * FROM " + TABLE_STALLS + " WHERE " + KEY_ID + " = " + stall_id;
+		
+		Log.e(LOG, selectQuery);
+		
+		Cursor c = db.rawQuery(selectQuery, null);
+		
+		if (c != null)
+			c.moveToFirst();
+		
+		Stall stand = new Stall(c.getInt(c.getColumnIndex(KEY_ID)), c.getInt(c.getColumnIndex(KEY_STALL_NUMBER)), 
+				c.getInt(c.getColumnIndex(KEY_STALL_BALANCE)));
+		
+		return stand;
+		
+	}
+	
+	// fetch all Stalls
+	public List<Stall> getAllStalls() {
+		List<Stall> stalls = new ArrayList<Stall>();
+		String selectQuery = "SELECT * FROM " + TABLE_STALLS;
+		
+		Log.e(LOG, selectQuery);
+		
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.rawQuery(selectQuery, null);
+		
+		// adding rows to list
+		if (c.moveToFirst()) {
+			do{
+				Stall stand = new Stall(c.getInt(c.getColumnIndex(KEY_ID)), c.getInt(c.getColumnIndex(KEY_STALL_NUMBER)), 
+				c.getInt(c.getColumnIndex(KEY_STALL_BALANCE)));
+				
+				stalls.add(stand);
+			} while (c.moveToNext());
+		}
+		return stalls;
+	}
+	
+	// Update a stall
+	public int updateStall(Stall stand){
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(KEY_STALL_NUMBER, stand.getNumber());
+		values.put(KEY_STALL_BALANCE, stand.getBalance());
+		
+		// update row
+		return db.update(TABLE_STALLS, values, KEY_ID + " = ?", new String[] {String.valueOf(stand.getId()) });
+	}
+	
+	// Delete a stall
+	public void deleteStall(long stall_id) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(TABLE_STALLS, KEY_ID + " = ?", new String[] {String.valueOf(stall_id)});
+	}
+	
+	
+	//creating a product INCOMPLETE
+	public long createProduct(Product item){
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put(KEY_PRODUCT_CODE, item.getCode());
+		values.put(KEY_PRODUCT_PRICE, item.getPrice());
+		
+		long product_id = db.insert(TABLE_PRODUCTS, null, values);
+		
+		return product_id;
+		
 	}
 
 }
