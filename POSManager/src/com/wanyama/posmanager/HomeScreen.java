74eb5e4798792d813bce6ee8a4ed092b8ab10fed
wyllos <will.i.yeezy@gmail.com>
2014-04-09 20:04:21 +0200

    [ADD] homescreen activity class, displays toast when mode selected

---
+++ POSManager/src/com/wanyama/posmanager/HomeScreen.java
@@ -0,0 +1,41 @@
+package com.wanyama.posmanager;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomeScreen extends Activity {
+
+	Context ctx;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mode);
+		ctx = this;
+		
+		TextView header = (TextView) findViewById(R.id.header);
+		Button master = (Button) findViewById(R.id.master);
+		Button slave = (Button) findViewById(R.id.slave);
+		
+		master.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(ctx, "Master Mode Selected", Toast.LENGTH_LONG).show();	
+			}
+		});
+		
+		slave.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(ctx, "Slave Mode Selected", Toast.LENGTH_LONG).show();	
+			}
+		});
+	}
+
+}
