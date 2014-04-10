1c98b812e185b1dc6a658744ceb374d7b364771e
wyllos <will.i.yeezy@gmail.com>
2014-04-10 02:30:43 +0200

    anonymous commit

--- POSManager/src/com/wanyama/posmanager/HomeScreen.java
+++ POSManager/src/com/wanyama/posmanager/HomeScreen.java
@@ -11,29 +11,32 @@ import android.widget.Toast;
 public class HomeScreen extends Activity {
 
 	Context ctx;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mode);
 		ctx = this;
-		
+
 		TextView header = (TextView) findViewById(R.id.header);
 		Button master = (Button) findViewById(R.id.master);
 		Button slave = (Button) findViewById(R.id.slave);
-		
+
 		master.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(ctx, "Master Mode Selected", Toast.LENGTH_LONG).show();	
+				Toast.makeText(ctx, "Master Mode Selected", Toast.LENGTH_LONG)
+						.show();
 			}
 		});
-		
+
 		slave.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(ctx, "Slave Mode Selected", Toast.LENGTH_LONG).show();	
+				Toast.makeText(ctx, "Slave Mode Selected", Toast.LENGTH_LONG)
+						.show();
 			}
 		});
 	}
