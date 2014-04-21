d552c693bf0b1881eede82d9a8ef50192d570280
wyllos <will.i.yeezy@gmail.com>
2014-04-22 00:30:35 +0200

    [ADD] displays list of products

--- POSManager/src/com/wanyama/configuration/EditProduct.java
+++ POSManager/src/com/wanyama/configuration/EditProduct.java
@@ -1,16 +1,47 @@
 package com.wanyama.configuration;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EditProduct extends Activity {
+import com.wanyama.helper.MasterDatabaseAdapter;
+import com.wanyama.model.Product;
+import com.wanyama.posmanager.R;
 
-	/** Called when the activity is first created. */
+public class EditProduct extends Activity implements AdapterView.OnItemClickListener {
+
+	// instance variables
+	private MasterDatabaseAdapter db;
+	private Context ctx;
+	private ListView list;
+	private List<Product> listArray;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.list_products);
 	
-	    // TODO Auto-generated method stub
+	    list = (ListView) findViewById(R.id.list_view);
+	    ctx = this;
+	    db = new MasterDatabaseAdapter(ctx);
+	    listArray = db.getAllProducts();
+	    
+	    ArrayAdapter<Product> adapter = new ArrayAdapter<Product>(ctx, android.R.layout.simple_list_item_1, listArray);
+	    list.setAdapter(adapter);
+	    list.setOnItemClickListener(this);
 	}
-
+	
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+	
+		
+	}
 }
