92cea86e02383db82cd5b2b89ae75f531723dc73
wyllos <will.i.yeezy@gmail.com>
2014-04-23 16:20:06 +0200

    [ADD] skeleton of slave setup menu

---
+++ POSManager/src/com/wanyama/configuration/SlaveMain.java
@@ -0,0 +1,17 @@
+package com.wanyama.configuration;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SlaveMain extends Activity {
+
+	private Button menuReturn;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
