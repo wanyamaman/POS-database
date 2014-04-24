0aee7e974393deacc119d1d1edd4a3d3b4551ee3
wyllos <will.i.yeezy@gmail.com>
2014-04-24 18:55:12 +0200

    [ADD] basic template of checkout basket page

---
+++ POSManager/src/com/wanyama/posmanager/Checkout.java
@@ -0,0 +1,16 @@
+package com.wanyama.posmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Checkout extends Activity {
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
