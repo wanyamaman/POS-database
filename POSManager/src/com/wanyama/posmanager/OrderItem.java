5983ede6cb45dc26300d29ef384506d7fa1a238e
wyllos <will.i.yeezy@gmail.com>
2014-04-25 05:52:29 +0200

    [UPDATE] updates display info. quantity still to be done

--- POSManager/src/com/wanyama/posmanager/OrderItem.java
+++ POSManager/src/com/wanyama/posmanager/OrderItem.java
@@ -3,16 +3,21 @@ package com.wanyama.posmanager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.wanyama.configuration.InsertProduct;
 import com.wanyama.configuration.SlaveMain;
 import com.wanyama.helper.MasterDatabaseAdapter;
+import com.wanyama.model.Product;
 
 public class OrderItem extends Activity {
 
+	// layout variables
 	private Button add;
 	private Button delete;
 	private Button menuReturn;
@@ -21,22 +26,38 @@ public class OrderItem extends Activity {
 	private TextView vCode;
 	private TextView  vPrice;
 	private TextView vQuantity;
+	// stall variables
+	private int stall_number;
+	// product variables
+	private Product product;
+	private String product_code;
 	private int code;
 	private int price;
+	// purchase variables
 	private int quantity;
-	private MasterDatabaseAdapter db;
+	//database variables
+	private MasterDatabaseAdapter dbAdapter;
+	
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.order_item);
-	
+	    
+	    Log.i("OrderItem", "beginning of onCreate");
+	    
 	    Intent i = getIntent();
         // getting attached intent data
-        String product_code = i.getStringExtra("code");
+        product_code = i.getStringExtra("code");
         
-        // initiate database
-        db = new MasterDatabaseAdapter(getApplicationContext());
+        Log.i("OrderItem", "fetched code from intent: "+product_code);
+        
+        // initialize database
+        dbAdapter = new MasterDatabaseAdapter(getApplicationContext());
+        setupDatabaseValues();
+        setupProductValues();
+        orderCount();
         
         // initiate layout variables
         add = (Button) findViewById(R.id.addBtn);
@@ -44,6 +65,14 @@ public class OrderItem extends Activity {
         menuReturn = (Button) findViewById(R.id.menuReturnBtn);
         checkoutBtn = (Button) findViewById(R.id.checkoutBtn);
         setupScreenBtn = (Button) findViewById(R.id.setupReturnBtn);
+        vCode = (TextView) findViewById(R.id.code);
+        vPrice = (TextView) findViewById(R.id.price);
+        vQuantity = (TextView) findViewById(R.id.quantity);
+        
+        Log.i("OrderItem", "Initialised layout");
+        
+        // update layout values
+        updateDisplay();
         
         // makes an order (adds to purchase table)
         add.setOnClickListener(new OnClickListener() {
@@ -70,6 +99,7 @@ public class OrderItem extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				dbAdapter.closeDB();
 				Intent launchProductMenu = new Intent(OrderItem.this,
 						ProductMenu.class);
 				startActivity(launchProductMenu);
@@ -82,6 +112,7 @@ public class OrderItem extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				dbAdapter.closeDB();
 				Intent launchCheckoutScreen = new Intent(OrderItem.this,
 						Checkout.class);
 				startActivity(launchCheckoutScreen);
@@ -94,6 +125,7 @@ public class OrderItem extends Activity {
 			
 			@Override
 			public void onClick(View v) {
+				dbAdapter.closeDB();
 				Intent launchSetupScreen = new Intent(OrderItem.this,
 						SlaveMain.class);
 				startActivity(launchSetupScreen);
@@ -101,7 +133,69 @@ public class OrderItem extends Activity {
 			}
 		});
         
+        dbAdapter.closeDB();
         
+	}
+	
+	// updates price and quantity values on layout
+	public void updateDisplay(){
+		// set vCode value
+		vCode.setText(String.valueOf(code)); 
+		// set vPrice value
+		vPrice.setText("R"+String.valueOf(price));
+		// set vQuantity value
+		vQuantity.setText(String.valueOf(quantity));
+	}
+	
+	// counts the number of orders of this product
+	public void orderCount(){
+		// set quantity value
+		quantity = 0; // replace
+	}
+	
+	// setup stall and product data
+	public void setupDatabaseValues(){
+		
+		if (dbAdapter.countStalls() <=0){
+        	// force user to add stall number
+        	Toast.makeText(getApplicationContext(), "Please insert a stall number!", Toast.LENGTH_LONG).show();
+        	dbAdapter.closeDB();
+        	Intent launchSetup = new Intent(OrderItem.this, SlaveMain.class);
+        	startActivity(launchSetup);
+        }
+        else if (dbAdapter.countProducts() <=0){
+        	// force user to add products
+        	Toast.makeText(getApplicationContext(), "Please add products to the database!", Toast.LENGTH_LONG).show();
+        	dbAdapter.closeDB();
+        	Intent launchAddProduct = new Intent(OrderItem.this, InsertProduct.class);
+        	startActivity(launchAddProduct);
+        } else {
+        	// get stall identity information
+        	stall_number = dbAdapter.getAllStalls().get(0).getNumber();
+        	Log.i("OrderItem", "stall number: "+stall_number);
+        	
+        }
+	}
+	
+	public void setupProductValues(){
+		
+		// set product code
+		try {
+		 code =	Integer.parseInt(product_code);
+		} catch (NumberFormatException e) {
+			Log.d("OrderItem", "unable to convert product code to integer value");
+			e.printStackTrace();
+			code = 911;
+		}
+	//	Toast.makeText(getApplicationContext(), String.valueOf(code), Toast.LENGTH_LONG).show();
+		
+		// set product price
+		product = dbAdapter.getProductByCode(code);
+		Log.v("OrderItem", product.toString());
+		price = product.getPrice();
+		
+		// count & set number of orders
+		
 	}
 
 }
